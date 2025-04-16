cca5f36ae107ed41c9a149e3856850bfabfef289
Talha Iqbal <talha.iqbal@mergestack.com>
2025-04-16 15:32:24 +0500

    Setup react tanstack router and absolute paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-)
+import '@src/index.css'
+// Import the generated route tree
+import { routeTree } from '@src/routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement)
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	)
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			'@src': path.resolve(__dirname, './src'),
+		},
+	},
 })
