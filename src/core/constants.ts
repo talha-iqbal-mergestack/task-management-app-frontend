5f09a281c1dd454cd7a15fd3df9db38584e204f7
Talha Iqbal <talha.iqbal@mergestack.com>
2025-04-30 11:14:40 +0500

    Used all-caps for constants

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,6 +1,7 @@
-export const invalidPasswordError =
+export const INVALID_PASSWORD_ERROR =
 	'Password must be at least 8 characters long'
-export const nonSimilarPasswordsError = "Passwords don't match"
-export const invalidUsernameError = 'Please select a username'
-export const invalidContactNumberError =
+export const NON_SIMILAR_PASSWORDS_ERROR = "Passwords don't match"
+export const INVALID_USERNAME_ERROR = 'Please select a username'
+export const INVALID_CONTACT_NUMBER_ERROR =
 	'Please select a valid contact number format'
+export const MISSING_TASK_NAME_ERROR = 'Task name is required'
--- src/core/schemas.ts
+++ src/core/schemas.ts
@@ -1,32 +1,33 @@
 import { string, object, boolean } from 'zod'
 
 import {
-	invalidContactNumberError,
-	invalidPasswordError,
-	invalidUsernameError,
-	nonSimilarPasswordsError,
+	INVALID_CONTACT_NUMBER_ERROR,
+	INVALID_PASSWORD_ERROR,
+	INVALID_USERNAME_ERROR,
+	MISSING_TASK_NAME_ERROR,
+	NON_SIMILAR_PASSWORDS_ERROR,
 } from '@src/core/constants'
 
 export const signinSchema = object({
 	email: string().email(),
-	password: string().min(8, invalidPasswordError),
+	password: string().min(8, INVALID_PASSWORD_ERROR),
 })
 
 export const signupSchema = object({
 	email: string().email(),
-	username: string().min(1, invalidUsernameError),
+	username: string().min(1, INVALID_USERNAME_ERROR),
 	contactNumber: string().regex(
 		/^(0\d{10}|9\d{11})$/,
-		invalidContactNumberError,
+		INVALID_CONTACT_NUMBER_ERROR,
 	),
-	password: string().min(8, invalidPasswordError),
-	confirmPassword: string().min(8, invalidPasswordError),
+	password: string().min(8, INVALID_PASSWORD_ERROR),
+	confirmPassword: string().min(8, INVALID_PASSWORD_ERROR),
 }).refine(data => data.password === data.confirmPassword, {
-	message: nonSimilarPasswordsError,
+	message: NON_SIMILAR_PASSWORDS_ERROR,
 	path: ['confirmPassword'],
 })
 
 export const createTaskSchema = object({
-	name: string().min(1, 'Task name is required'),
+	name: string().min(1, MISSING_TASK_NAME_ERROR),
 	completed: boolean(),
 })
